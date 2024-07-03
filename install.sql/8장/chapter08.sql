ac405bd9ddf865b2b7e36f75a846842872b221ec
soryeon kim <kimsr9210@naver.com>
2024-07-03 22:18:39 +0900

    chapter08

---
+++ install.sql/8장/chapter08.sql
@@ -0,0 +1,33 @@
+--테이블 생성
+CREATE TABLE emp08 (
+    EMP_ID02 NUMBER NOT NULL,
+    EMP_NAME VARCHAR2(100) NOT NULL,
+    GENDER VARCHAR(10),
+    AGE NUMBER,
+    HIRE_DATE DATE,
+    ETC VARCHAR2(300),
+    CONSTRAINT EMP08_PK PRIMARY KEY (EMP_ID02)
+);
+
+--컬럼 수정
+ALTER TABLE emp08 RENAME COLUMN EMP_NAME TO EMP_NAME2;
+
+SELECT * FROM emp08;
+
+--데이터 삽입
+INSERT INTO emp08 ( emp_id2, emp_name2, gender, age, hire_date )
+VALUES (1, '선덕여왕', '여성', 23, TO_DATE('2018-02-01', 'YYYY-MM-DD'));
+
+INSERT INTO emp08 ( emp_id2, emp_name2, gender, age, hire_date )
+VALUES (2, '허난설헌', '여성', 33, TO_DATE('2018-02-01', 'YYYY-MM-DD'));
+
+INSERT INTO emp08 ( emp_id2, emp_name2, gender, age, hire_date )
+VALUES (3, '김만덕', '여성', 43, TO_DATE('2018-02-01', 'YYYY-MM-DD'));
+
+INSERT INTO emp08 ( emp_id2, emp_name2, gender, age, hire_date )
+VALUES (4, '장희빈', '여성', 35, TO_DATE('2018-02-01', 'YYYY-MM-DD'));
+
+INSERT INTO emp08 ( emp_id2, emp_name2, gender, age, hire_date )
+VALUES (5, '신사임당', '여성', 45, TO_DATE('2018-02-01', 'YYYY-MM-DD'));
+
+COMMIT;
